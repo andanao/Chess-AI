7679d96ae078062eeb35f83fc177ee6edeca4866
andanao <47489123+andanao@users.noreply.github.com>
2019-10-19 10:19:58 -0400

    trying to figure out what is going on with my python distro

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-import chess
-import chess.engine
+# import chess
+# import chess.engine
 
-engine = chess.engine.SimpleEngine.popen_uci("/usr/bin/stockfish")
+# # engine = chess.engine.SimpleEngine.popen_uci("/usr/bin/stockfish")
 
-board = chess.Board()
-while not board.is_game_over():
-    result = engine.play(board, chess.engine.Limit(time=0.100))
-    board.push(result.move)
+# board = chess.Board()
+# while not board.is_game_over():
+#     result = engine.play(board, chess.engine.Limit(time=0.100))
+#     board.push(result.move)
 
-engine.quit()
+# engine.quit()
+
+print('test')
