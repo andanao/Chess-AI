e3122eac471778cd6396bc0947b3a3d7aeedd844
andanao <47489123+andanao@users.noreply.github.com>
2019-10-19 14:09:03 -0400

    helping sleepy

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-# import chess
-# import chess.engine
+import chess
+import chess.engine
+import chess.pgn
 
-# # engine = chess.engine.SimpleEngine.popen_uci("/usr/bin/stockfish")
+# engine = chess.engine.SimpleEngine.popen_uci("C:\\Users\\Adrian\\Downloads\\stockfish-10-win\\stockfish-10-win\\Windows\\stockfish_10_x64")
 
 # board = chess.Board()
 # while not board.is_game_over():
@@ -10,4 +11,27 @@
 
 # engine.quit()
 
-print('test')
+
+board  = chess.Board()
+board.legal_moves 
+
+chess.Move.from_uci("a8a1") in board.legal_moves
+
+
+board.push_san("e4")
+
+board.push_san("e5")
+
+board.push_san("Qh5")
+
+board.push_san("Nc6")
+
+board.push_san("Bc4")
+
+board.push_san("Nf6")
+
+board.push_san("Qxf7")
+
+board.is_checkmate()
+
+print(board)
