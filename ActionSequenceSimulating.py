a7c7eb4c343b99d5db679ad3d02fcb930b3f3a42
andanao <47489123+andanao@users.noreply.github.com>
2019-10-20 14:03:12 -0400

    ASS engine works!!!

--- ActionSequenceSimulating.py
+++ ActionSequenceSimulating.py
@@ -14,26 +14,38 @@ class engine:
         else:
             player_col = -1
         
-        best_score = [-10000] #set the bar low
         root = self.help.grow_twigs(board,player_col)
 
-        movespace = []
         #below gets the best move
-        if self.turn < 1:
-            test_space = []
-            for cur in root.variations:
-                child = self.help.grow_twigs(cur.board(),-player_col)
-                for item in child.variations:
-                    test_space.append(item.comment)
-            print(len(test_space))
-
+        # if self.turn < 1:
+        test_space = []
         for cur in root.variations:
-            if cur.comment[1] >= best_score[0]:
-                if cur.comment[1] > best_score[0]:
+            child = self.help.grow_twigs(cur.board(),-player_col)
+            for item in child.variations:
+                test_space.append([cur.comment[0], cur.comment[1] - item.comment[1]])
+        
+        
+        best_score = [-10000] #set the bar low
+        movespace = []
+        for i in range(len(test_space)):
+            if test_space[i][1] >= best_score[0]:
+                if test_space[i][1] > best_score[0]:
                     best_score.clear()
                     movespace.clear()
-                best_score.append(cur.comment[1])
-                movespace.append(cur.comment[0])
+                best_score.append(test_space[i][1])
+                movespace.append(test_space[i][0])
+                
+            # print(test_space[i])
+        # print(max(test_space))
+
+            # print(len(test_space))
+        # for cur in root.variations:
+        #     if cur.comment[1] >= best_score[0]:
+        #         if cur.comment[1] > best_score[0]:
+        #             best_score.clear()
+        #             movespace.clear()
+        #         best_score.append(cur.comment[1])
+        #         movespace.append(cur.comment[0])
         best_move = random.choice(movespace)
         self.turn += 1
         # print(type(best_node.move))
