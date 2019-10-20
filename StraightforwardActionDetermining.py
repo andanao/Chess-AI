60ad586a6a5def414d2d8bd9d853ade490a60b64
andanao <47489123+andanao@users.noreply.github.com>
2019-10-20 06:48:20 -0400

    changes to temp

--- StraightforwardActionDetermining.py
+++ StraightforwardActionDetermining.py
@@ -8,12 +8,15 @@ class engine:
         self.help = helper.meth()
     
     def play(self, board, tlim):
+        # if board.turn == BLACK:
+        #     board.mirror() #this no worky work
+        
         move_list = self.help.legal_move_list(board)
         
         root = chess.pgn.Game()
         root.setup(board.fen())
 
-        best_score = -10
+        best_score = [-10000] #set the bar low
         list_analysis = []
         for item in move_list:
             child = chess.pgn.GameNode()
@@ -21,19 +24,16 @@ class engine:
             child.move = board.parse_san(item)
             score = self.help.board_value(child.board(),1)
             child.comment = score
-            list_analysis.append([child.move, score])
-            if score > best_score:
-                best_score = score
-                best_move = child.move
-                
-
+            list_analysis.append([child.move, score]) #idk if this is neccessary
             root.variations.append(child)
 
         if self.turn < 2:
             print(list_analysis)
 
+
         for cur in root.variations:
-            pass
+            if cur.comment >= best_score:
+                # add to list if > 1 or equal
         
         self.turn += 1
         # print(type(best_node.move))
--- temp.py
+++ temp.py
@@ -3,9 +3,6 @@ import re
 import random
 import helper
 
-print('\n\n\n\n\n')
-board  = chess.Board()
-board.set_epd("1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - bm Qd1+; id \"BK.01\";")
-chelp = helper.meth()
-bval = chelp.board_value(board,1)
-print(bval)
+test = [1000, 1000, 1000]
+a = 2000
+
