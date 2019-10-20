a737c96e8b0a1e52124a976eb9cec04baa452bf3
andanao <47489123+andanao@users.noreply.github.com>
2019-10-20 02:05:23 -0400

    kinda got peice value working

--- temp.py
+++ temp.py
@@ -4,10 +4,15 @@ import random
 import helper
 
 # print('\n\n\n\n\n')
+piece_val = { 'P': 100, 'N': 250, 'B': 300, 'R': 500, 'Q': 1000, 'K': 100000, 'p': -100, 'n': -250, 'b': -300, 'r': -500, 'q': -1000, 'k': -100000}
 board  = chess.Board()
 pmap = board.piece_map()
+value = 0
 for key in pmap:
-    print(key)
+    # print(key)
+    value += piece_val[pmap[key].symbol()]
+    # print(pmap[key].symbol())
+print(value)
 # print(board.piece_map())
 # print(board)
 # print(pmap['0'])
