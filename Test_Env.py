4ed13f72bf795c8fb6334427b000a3445c3aceb5
andanao <47489123+andanao@users.noreply.github.com>
2020-06-20 14:27:48 -0400

    Hi Howard, i'm test environment

--- Test_Env.py
+++ Test_Env.py
@@ -4,15 +4,19 @@ import math
 import time
 
 import loopy as eng1
-import loopy as eng2
-
-# import twiggy as eng2
-# import RandomTake as eng1
+# import RandomEngine as eng2
+# import rematch2 as eng1
+import rematch3 as eng2
 
 debug = open("game_debug.txt", "w")
 stack = open("game_stack.pgn", "w")
 
+white_error_time = 0
+black_error_time = 0
+
 def take_turn(board, engine):
+    global black_error_time
+    global white_error_time
     color = "White"
     if board.turn == chess.BLACK:
         color = "Black"
@@ -21,23 +25,32 @@ def take_turn(board, engine):
     end_time = time.time()
     if end_time - start_time > tlim:
         print("went over time by " + str(end_time - start_time - tlim) + " sec")
-    if board.uci(result) == "0000":
-        print(color + " null")
+        if board.turn:
+            white_error_time += end_time - start_time - tlim
+        else:
+            black_error_time += end_time - start_time - tlim
+
+    try:
+        if board.uci(result) == "0000":
+            print(color + " null")
+            return None
+        board.push(result)
+        debug.write("\n\n" + color + "\n" + board.uci(result) + "\n" + str(board))
+        stack.write(board.uci(result)+"\n")
+        print(result)
+        return board
+    except:
+        print(color + " tried illegal move")
         return None
-    board.push(result)
-    debug.write("\n\n" + color + "\n" + board.uci(result) + "\n" + str(board))
-    stack.write(board.uci(result)+"\n")
-    print(result)
-    return board
+
 
 if __name__ == '__main__':
-    tlim = 1
-
-    print("\n\n\n\n\n\n\n\t----\tStarting\t----")
+    tlim = 10
+    print("\n\n\n\n\n\n\n")
     white_engine = eng1.engine(tlim)
     black_engine = eng2.engine(tlim)
     engines = {True : white_engine, False: black_engine}
-
+        
     board = chess.Board()
 
     while not board.is_game_over():
@@ -46,8 +59,14 @@ if __name__ == '__main__':
             break
         board = temp_board
         if board.is_game_over():
-            break 
+            break
 
-
+    debug.close()
+    stack.close()
     print('\n\nGG!')
-    print(str(board.result())+' in '+str(math.ceil(len(board.move_stack)/2))+'\n\n')
+    print('White went over by '+ str(white_error_time)+ ' seconds')
+    print('Black went over by '+ str(black_error_time)+ ' seconds')
+    try:
+        print(str(board.result())+' in '+ str(math.ceil(len(board.move_stack)/2))+'\n\n')
+    except:
+        pass
